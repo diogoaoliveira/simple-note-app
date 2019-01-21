8799371c56ea443f73bdefa77cf3f767c9482c30
Diogo Augusto <diogo.augustoo@gmail.com>
2019-01-21 12:22:59 -0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-weight: 700;
+    text-align: center;
+`;
+
+const App = () => <Title>Simple Notes</Title>;
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+const saga = createSagaMiddleware();
+
+const middleware = applyMiddleware(saga, logger);
+
+const reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+    rootReducer,
+    compose(
+        middleware,
+        reduxDevTools
+    )
+);
+
+saga.run(rootSaga);
+
+export default store;
